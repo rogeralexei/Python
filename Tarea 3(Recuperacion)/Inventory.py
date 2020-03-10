9619b0e20e811f236618f16971ed1710dc98318c
AdrienV17 <45920897+AdrienV17@users.noreply.github.com>
2020-03-10 00:21:09 -0500

    Continuing Inventory

--- Tarea 3(Recuperacion)/Inventory.py
+++ Tarea 3(Recuperacion)/Inventory.py
@@ -7,7 +7,12 @@ class Usuario:
     def __init__(self,nombre):
         self.nombre=nombre
 lista={}
-listaDeUsuarios=open("Usuarios.txt","r").read().split('\n')
+listaDeUsuarios=open("Usuarios.txt","a+").read().split('\n')
+for usuario in listaDeUsuarios:
+        if usuario in lista.keys():
+            lista[usuario]+=1
+        else:
+            lista[usuario]=1
 if __name__:"__main__"
 # Creating a Registration Class
 print("Hola, te gustaria registrarte?\n")
@@ -19,9 +24,8 @@ registration=int(input(("Seleccione la opcion(numero) deseada: ")))
 
 if registration==1:
     print("\nDe acuerdo, vamos a registrarte...\n")
-    Usuario.nombre=input('Dime tu Nombre: ')
-    if Usuario.nombre in lista.keys():
-         print("Ese usuario no esta disponible")
-    else:
-        lista[usuarios]=1
-  
+    Usuario.nombre=input('Dame un nombre de usuario: ')
+    while(Usuario.nombre in lista.keys()):
+        usuario=input('Ese usuario ya esta registrado; porfavor escribe otro: ')
+    lista.write("\n"+usuario)
+
